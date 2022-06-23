5960a608b15f6ecd21cce5eba8af32224baa3f0c
Brooks Adams <aba1421@jagmail.southalabama.edu>
2022-06-22 23:23:25 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MethodsEx
+{
+    internal class Program
+    {
+        public static void MadLib()
+        {
+            Console.WriteLine("What is your name?");
+            var fullName = Console.ReadLine();
+
+            Console.WriteLine($"Hey {fullName} What's your favorite color?");
+            var faveColor = Console.ReadLine();
+
+            Console.WriteLine("What's your favorite Animal");
+            var faveAnimal = Console.ReadLine();
+
+            Console.WriteLine("What's your favorite band?");
+            var faveBand = Console.ReadLine();
+
+            Console.WriteLine($"One day, {fullName} was walking through the woods wearing a {faveColor} shirt. " +
+                $"Suddenly, a {faveAnimal} appeared with an iPod. " +
+                $"It was listening to {faveBand}. {fullName} was surprised it liked {faveBand}, too! ");
+        }
+        static void Main(string[] args)
+        {
+            //MadLib();
+
+            Console.WriteLine(Add(6, 7));
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
